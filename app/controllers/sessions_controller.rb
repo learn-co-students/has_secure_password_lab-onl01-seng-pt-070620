f5d233185d4f019ceff0ce0f58a5ef7014c56c2b
katiethile <lethikatie@gmail.com>
2020-12-08 15:11:08 -0800

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+    def new 
+     @user = User.new 
+    end 
+
+    def create 
+       user = User.find_by(name: params[:user][:name])
+       authenticated = user.try(:authenticate, params[:user][:password])
+       return head(:forbidden) unless authenticated
+       @user = user 
+       session[:user_id] = @user.id 
+    end 
+
+    def destroy 
+        session.destroy :name 
+    end 
+end 
